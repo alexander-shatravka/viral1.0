10e75e4f8df2671e29eb0d124fb1e9e0fdc4ec90
Indiel <kori.yanagi@gmail.com>
2018-10-19 13:25:33 +0300

    progress bar

--- js/menu-script.js
+++ js/menu-script.js
@@ -80,6 +80,11 @@
                 });
             });
         });
+
+        window.addEventListener('resize', () => {
+            let vh = window.innerHeight * 0.01;
+            document.documentElement.style.setProperty('--vh', `${vh}px`);
+        });
     }
 
 
